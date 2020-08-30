32883734d185b542d8fc198df58d96dd5c5eb27c
harutotanabe09@gmail.com <harutotanabe09@gmail.com>
2020-08-30 16:48:04 +0900

    :sparkles: (add optional)

--- src/main/java/co/jp/foobar/controller/SkeltonController.java
+++ src/main/java/co/jp/foobar/controller/SkeltonController.java
@@ -3,12 +3,14 @@ package co.jp.foobar.controller;
 import co.jp.foobar.entity.Staff;
 import co.jp.foobar.service.SkeltonService;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class SkeltonController {
 
   @Autowired SkeltonService service;
@@ -16,7 +18,11 @@ public class SkeltonController {
   @GetMapping("/skelton")
   public String index(Model model) {
     Optional<Staff> data = service.findOne(1);
-    model.addAttribute("msg", "get::" + data.get().getFirstName());
+    if (data.isPresent()) {
+      model.addAttribute("msg", "get::" + data.get().getFirstName());
+    } else {
+      log.error("massage is null");
+    }
     return "index";
   }
 }
